f21f164607b4123b891df9ecbbb4e97121559482
laurampeixoto <laura.peixoto@estudante.ifms.edu.br>
2025-11-25 20:18:47 -0400

    Fiz o doubleclick nas tela de pesquisas, está tudo funcionando bem aparentemente. Acho que acabei o trabalho então.

--- src/view/LmpJDlgPesquisarVendas.java
+++ src/view/LmpJDlgPesquisarVendas.java
@@ -61,6 +61,11 @@ public class LmpJDlgPesquisarVendas extends javax.swing.JDialog {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
 
         jBtnOk.setText("OK");
@@ -100,6 +105,13 @@ public class LmpJDlgPesquisarVendas extends javax.swing.JDialog {
         LmpjDlgVendas.beanView(lmpVendas);
         this.setVisible(false);
     }//GEN-LAST:event_jBtnOkActionPerformed
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        // TODO add your handling code here:
+        if (evt.getClickCount() == 2) {
+            jBtnOkActionPerformed(null);
+        }
+    }//GEN-LAST:event_jTable1MouseClicked
 
     /**
      * @param args the command line arguments
